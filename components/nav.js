8736add1685967286d6b3b06e933080ec352d383
dev-azeezat <azeezatadebola87@gmail.com>
2022-12-20 22:09:11 +0100

    @media query completed

--- components/nav.js
+++ components/nav.js
@@ -5,7 +5,7 @@ import {useState} from 'react'
 export default function Nav() {
   const [navbar, setNavbar] = useState(false)
  return (
-   <nav className='fixed top-0 flex items-center justify-between w-full py-5 overflow-hidden bg-white px-14 shadow-[0_10px_100px_rgb(0_0_0_/_10%)] z-[1000] md:overflow-visible'>
+   <nav className='fixed top-0 flex items-center justify-between w-full py-5 overflow-hidden bg-white px-14 shadow-[0_10px_100px_rgb(0_0_0_/_10%)] z-[1000] md:overflow-visible md:py-6'>
      <div className='md:flex md:items-center'>
        <div className='flex items-center justify-center cursor-pointer'>
          <Image
@@ -20,7 +20,7 @@ export default function Nav() {
            AZEEZAT OLAITAN
          </span>
        </div>
-       <div className='hidden md:block md:relative md:left-72'>
+       <div className='hidden md:block md:relative md:left-[300px]'>
          <button className='' onClick={() => setNavbar(!navbar)}>
            {navbar ? (
              <svg
@@ -57,21 +57,21 @@ export default function Nav() {
        </div>
      </div>
      <div
-       className={`md:mt-[5rem] md:absolute md:top-14 md:left-0 md:bg-white md:w-full ${
+       className={`md:mt-[5rem] md:absolute md:top-14 md:left-0 md:bg-white md:w-full md:${
          navbar ? 'block' : 'hidden'
        }`}
      >
        <ul className='flex cursor-pointer md:grid md:gap-y-8'>
-         <li className='mr-14 text-[15px] font-bold uppercase tracking-wider md:block md:px-[2rem] md:py-[3rem] md:text-[1.6rem] md:text-[#333] md:text-right md:border-y md:border-solid md:border-black md:mr-0 transition duration-300 ease-linear hover:text-rose-400 '>
+         <li className='mr-14 text-[15px] font-bold uppercase tracking-wider md:block md:px-[2rem] md:py-[2.8rem] md:text-[1.6rem] md:text-[#333] md:text-right md:border-y md:border-solid md:border-[#eee] md:mr-0 md:pr-20 transition duration-300 ease-linear hover:text-rose-400 '>
            <Link href='#home'>home</Link>
          </li>
-         <li className='mr-14 text-[15px] font-bold uppercase tracking-wider md:block md:px-[2.5rem] md:py-[3rem] md:text-[1.6rem] md:text-[#333] md:text-right md:border-b md:border-solid md:border[#eee] transition duration-300 ease-linear hover:text-rose-400'>
+         <li className='mr-14 text-[15px] font-bold uppercase tracking-wider md:block md:px-[2.5rem] md:py-[2.8rem] md:text-[1.6rem] md:text-[#333] md:text-right md:border-b md:border-solid md:border[#eee] md:mr-0 md:pr-20 md:-mt-8 transition duration-300 ease-linear hover:text-rose-400'>
            <Link href='#about'>about</Link>
          </li>
-         <li className='mr-14 font-bold uppercase tracking-wider md:block md:px-[2.5rem] md:py-[3rem] md:text-[1.6rem] md:text-[#333] md:text-right md:border-b md:border-solid md:border[#eee] transition duration-300 ease-linear text-[15px] hover:text-rose-400'>
+         <li className='mr-14 font-bold uppercase tracking-wider md:block md:px-[2.5rem] md:py-[2.8rem] md:text-[1.6rem] md:text-[#333] md:text-right md:border-b md:border-solid md:border[#eee] md:mr-0 md:pr-20 md:-mt-8 transition duration-300 ease-linear text-[15px] hover:text-rose-400'>
            <Link href='#project'>projects</Link>
          </li>
-         <li className='mr-8 font-bold uppercase tracking-wider md:block md:px-[2.5rem] md:py-[3rem] md:text-[1.6rem] md:text-[#333] md:text-right md:border-b md:border-solid md:border[#eee] transition duration-300 ease-linear text-[15px] hover:text-rose-400'>
+         <li className='mr-8 font-bold uppercase tracking-wider md:block md:px-[2.5rem] md:py-[2.8rem] md:text-[1.6rem] md:text-[#333] md:text-right md:border-b md:border-solid md:border[#eee] md:mr-0 md:pr-20 md:-mt-8 transition duration-300 ease-linear text-[15px] hover:text-rose-400'>
            <Link href='#contact'>contact</Link>
          </li>
        </ul>
