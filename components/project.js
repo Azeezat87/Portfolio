9d8a30095ca713f485a06550c97b41a8fab52e34
dev-azeezat <azeezatadebola87@gmail.com>
2023-07-07 12:29:31 +0100

    new changes

--- components/project.js
+++ components/project.js
@@ -28,6 +28,39 @@ export default function Project() {
            />
            <Image
              priority
+             src='/images/timmy.jpg'
+             className='relative top-[-54%] left-[15.7%] rounded h-[310px] w-[490px] md:bottom-[427px] md:left-[129px] md:h-[346px] md:w-[538px]'
+             height='0'
+             width='0'
+             sizes='100vw'
+             alt='project'
+           />
+         </div>
+         <div className='max-w-[450px] mt-12 md:-mt-56 md:max-w-[100%] md:flex md:flex-col md:items-center md:mx-10'>
+           <h3 className='font-bold text-2xl mb-6 md:text-[50px] md:mb-10'>
+             Timmy-Portfolio
+           </h3>
+           <p className='text-lg text-[#666] md:text-[32px] md:leading-[50px] md:text-center md:mb-5'>
+             I created this portfolio from scratch using next js and tailwind css along with the figma file presented by the product designer.
+           </p>
+           <button className='px-12 py-3 mt-8 text-lg font-semibold tracking-wider transition duration-300 ease-linear rounded-md shadow-[0_5px_15px_0_rgb(0_0_0_/_15%)] bg-rose-400 hover:-translate-y-1 uppercase md:mt-3 md:mb-[350px] md:text-3xl md:px-[70px] md:py-6 md:rounded-lg'>
+             case study
+           </button>
+         </div>
+       </div>
+
+       <div className='flex mb-[-200px] md:flex-col'>
+         <div className='mr-6 md:ml-6 md:w-[800px]md:h-[400px] md:-mb-14'>
+           <Image
+             priority
+             src='/images/laptop.png'
+             className='md:h-[100%] md:w-[100%] '
+             height={720}
+             width={720}
+             alt='project'
+           />
+           <Image
+             priority
              src='/images/devL.jpg'
              className='relative top-[-54%] left-[15.7%] rounded h-[310px] w-[490px] md:bottom-[427px] md:left-[129px] md:h-[346px] md:w-[538px]'
              height='0'
